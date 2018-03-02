0b38b3c9b43ea5c23d5b0387bb1c6aedd38cc8e0
norman lewis <takialog@gmail.com>
2018-03-02 15:26:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ def self.find_by_name(song_name)
 end
 
   def self.find_or_create_by_name(name)
-     self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+     self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
